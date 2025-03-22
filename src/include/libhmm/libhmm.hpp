6395d2319149cf77b58475630a0dcc737102b304
Jon Bringhurst <jon@bringhurst.org>
2025-03-21 17:14:35 -0700

    Initial file layout

---
+++ src/include/libhmm/libhmm.hpp
@@ -0,0 +1,8 @@
+#ifndef LIBHMM_LIBHMM_HPP_INCLUDED
+#defined LIBHMM_LIBHMM_HPP_INCLUDED
+
+namespace hmm {
+  void say_hello()
+}
+
+#endif /* LIBHMM_LIBHMM_HPP_INCLUDED */
---
+++ src/libhmm.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <libhmm/libhmm.hpp>
+
+void hmm::say_hello() {
+  std::cout << "Hello, hmm!\n";
+}
